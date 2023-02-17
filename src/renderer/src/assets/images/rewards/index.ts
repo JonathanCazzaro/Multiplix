aa0b382c46813b53fd7faefceb1b0817c74fe708
Jonathan Cazzaro <jonathan.cazzaro@gmail.com>
2023-02-17 17:20:58 +0100

    last modifications

---
+++ src/renderer/src/assets/images/rewards/index.ts
@@ -0,0 +1,114 @@
+import Reward_1 from './1.jpg';
+import Reward_1_Thumbnail from './thumbnails/1.jpg';
+
+import Reward_2 from './2.jpg';
+import Reward_2_Thumbnail from './thumbnails/2.jpg';
+
+import Reward_3 from './3.jpg';
+import Reward_3_Thumbnail from './thumbnails/3.jpg';
+
+import Reward_4 from './4.jpg';
+import Reward_4_Thumbnail from './thumbnails/4.jpg';
+
+import Reward_5 from './5.jpg';
+import Reward_5_Thumbnail from './thumbnails/5.jpg';
+
+import Reward_6 from './6.jpg';
+import Reward_6_Thumbnail from './thumbnails/6.jpg';
+
+import Reward_7 from './7.jpg';
+import Reward_7_Thumbnail from './thumbnails/7.jpg';
+
+import Reward_8 from './8.jpg';
+import Reward_8_Thumbnail from './thumbnails/8.jpg';
+
+import Reward_9 from './9.jpg';
+import Reward_9_Thumbnail from './thumbnails/9.jpg';
+
+import Reward_10 from './10.jpg';
+import Reward_10_Thumbnail from './thumbnails/10.jpg';
+
+import Reward_11 from './11.jpg';
+import Reward_11_Thumbnail from './thumbnails/11.jpg';
+
+import Reward_12 from './12.jpg';
+import Reward_12_Thumbnail from './thumbnails/12.jpg';
+
+import Reward_13 from './13.jpg';
+import Reward_13_Thumbnail from './thumbnails/13.jpg';
+
+import Reward_14 from './14.jpg';
+import Reward_14_Thumbnail from './thumbnails/14.jpg';
+
+import Reward_15 from './15.jpg';
+import Reward_15_Thumbnail from './thumbnails/15.jpg';
+
+import Reward_16 from './16.jpg';
+import Reward_16_Thumbnail from './thumbnails/16.jpg';
+
+import Reward_17 from './17.jpg';
+import Reward_17_Thumbnail from './thumbnails/17.jpg';
+
+import Reward_18 from './18.jpg';
+import Reward_18_Thumbnail from './thumbnails/18.jpg';
+
+import Reward_19 from './19.jpg';
+import Reward_19_Thumbnail from './thumbnails/19.jpg';
+
+import Reward_20 from './20.jpg';
+import Reward_20_Thumbnail from './thumbnails/20.jpg';
+
+import Reward_21 from './21.jpg';
+import Reward_21_Thumbnail from './thumbnails/21.jpg';
+
+import Reward_22 from './22.jpg';
+import Reward_22_Thumbnail from './thumbnails/22.jpg';
+
+import Reward_23 from './23.jpg';
+import Reward_23_Thumbnail from './thumbnails/23.jpg';
+
+import Reward_24 from './24.jpg';
+import Reward_24_Thumbnail from './thumbnails/24.jpg';
+
+import Reward_25 from './25.jpg';
+import Reward_25_Thumbnail from './thumbnails/25.jpg';
+
+import Reward_26 from './26.jpg';
+import Reward_26_Thumbnail from './thumbnails/26.jpg';
+
+import Reward_27 from './27.jpg';
+import Reward_27_Thumbnail from './thumbnails/27.jpg';
+
+import Reward_28 from './28.jpg';
+import Reward_28_Thumbnail from './thumbnails/28.jpg';
+
+export default [
+  { id: 1, src: Reward_1, thumbnail: Reward_1_Thumbnail },
+  { id: 2, src: Reward_2, thumbnail: Reward_2_Thumbnail },
+  { id: 3, src: Reward_3, thumbnail: Reward_3_Thumbnail },
+  { id: 4, src: Reward_4, thumbnail: Reward_4_Thumbnail },
+  { id: 5, src: Reward_5, thumbnail: Reward_5_Thumbnail },
+  { id: 6, src: Reward_6, thumbnail: Reward_6_Thumbnail },
+  { id: 7, src: Reward_7, thumbnail: Reward_7_Thumbnail },
+  { id: 8, src: Reward_8, thumbnail: Reward_8_Thumbnail },
+  { id: 9, src: Reward_9, thumbnail: Reward_9_Thumbnail },
+  { id: 10, src: Reward_10, thumbnail: Reward_10_Thumbnail },
+  { id: 11, src: Reward_11, thumbnail: Reward_11_Thumbnail },
+  { id: 12, src: Reward_12, thumbnail: Reward_12_Thumbnail },
+  { id: 13, src: Reward_13, thumbnail: Reward_13_Thumbnail },
+  { id: 14, src: Reward_14, thumbnail: Reward_14_Thumbnail },
+  { id: 15, src: Reward_15, thumbnail: Reward_15_Thumbnail },
+  { id: 16, src: Reward_16, thumbnail: Reward_16_Thumbnail },
+  { id: 17, src: Reward_17, thumbnail: Reward_17_Thumbnail },
+  { id: 18, src: Reward_18, thumbnail: Reward_18_Thumbnail },
+  { id: 19, src: Reward_19, thumbnail: Reward_19_Thumbnail },
+  { id: 20, src: Reward_20, thumbnail: Reward_20_Thumbnail },
+  { id: 21, src: Reward_21, thumbnail: Reward_21_Thumbnail },
+  { id: 22, src: Reward_22, thumbnail: Reward_22_Thumbnail },
+  { id: 23, src: Reward_23, thumbnail: Reward_23_Thumbnail },
+  { id: 24, src: Reward_24, thumbnail: Reward_24_Thumbnail },
+  { id: 25, src: Reward_25, thumbnail: Reward_25_Thumbnail },
+  { id: 26, src: Reward_26, thumbnail: Reward_26_Thumbnail },
+  { id: 27, src: Reward_27, thumbnail: Reward_27_Thumbnail },
+  { id: 28, src: Reward_28, thumbnail: Reward_28_Thumbnail }
+] as Multiplix.RewardPicture[];
--- src/renderer/src/components/GameInterface/GameInterfaceRewards/GameInterfaceRewards.tsx
+++ src/renderer/src/components/GameInterface/GameInterfaceRewards/GameInterfaceRewards.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Panel } from 'rsuite';
-import { useImagesImport } from '../../../hooks/useImagesImport';
 import user from '../../../services/user';
 import RewardsGrid from './RewardsGrid';
+import images from '../../../assets/images/rewards/index';
 
 const GameInterfaceRewards: React.FC = () => {
-  const images = useImagesImport();
-
   return (
     <Panel className="gameinterface__rewards">
       <h1>Bienvenue dans la salle des trophées !</h1>
